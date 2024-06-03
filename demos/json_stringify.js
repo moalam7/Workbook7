f839e538bdae55a468163d4465c6f689b4c520a2
moalam7 <mohammedalamx@gmail.com>
2024-06-03 13:04:15 -0400

    add notes

---
+++ demos/json_stringify.js
@@ -0,0 +1,17 @@
+"use strict";
+
+let job = {
+  title : "Web Designer",
+  startDate : "October 2022",
+  company : "AT&T",
+  minSalary : 52000,
+  maxSalary : 86000
+  };
+
+let str = JSON.stringify(job);
+// let str = JSON.stringify(job, null, 2);
+ console.log(str);
+
+//  let jobAsString = JSON.stringify();
+
+//  console.log(jobAsString);
